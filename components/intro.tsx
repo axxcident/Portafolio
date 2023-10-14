e2b2a4278b9530babf3c2ad314ae0c65b3703d6c
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-10-14 16:51:28 +0200

    1:33:54 börja skriv introText

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 
@@ -5,8 +7,38 @@ export default function intro() {
   return (
     <section>
       <div className="flex items-center justify-center">
-        <div>
-          <Image src="/me.jpg" alt="Axel Olivecrona" width="192" height="192" />
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="/me.jpg"
+              alt="Axel Olivecrona"
+              width="192"
+              height="192"
+              quality={95}
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-lg"
+            />
+            <motion.span
+              className="absolute bottom-0 right-0 text-4xl"
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 125,
+                delay: 0.1,
+                duration: 0.7,
+              }}
+            >
+              👋
+            </motion.span>
+          </motion.div>
         </div>
       </div>
     </section>
