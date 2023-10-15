ad3ed6d3d5c6274c6e1e7893f950e371d5dd4467
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-10-15 15:47:55 +0200

    Section divider 2:01:48

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,11 +1,22 @@
 "use client";
-import { motion } from "framer-motion";
+import { delay, motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { FaGithub, FaGithubSquare } from "react-icons/fa";
+import { HiDownload } from "react-icons/hi";
 
 export default function intro() {
+  // const { ref } = useSectionInView("Home", 0.5);
+  // const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
   return (
-    <section>
+    <section
+      // ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -41,6 +52,59 @@ export default function intro() {
           </motion.div>
         </div>
       </div>
+      {/* h1 eller paragraf */}
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-3xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Axel.</span> I'm a{" "}
+        <span className="font-bold">frontend developer</span> with junior
+        experience. I enjoy designing & building{" "}
+        <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.h1>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+        className="flex flex-col sm:flex-row justify-center items-center gap-2 px-4 text-lg font-medium"
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-900 active:scale-105 transition"
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          href="/Axel_O_CV.pdf"
+          download={true}
+          className="group bg-white px-7 py-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          Download CV
+          <HiDownload className="opacity-70 group-hover:translate-y-0.5 transition" />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/axel-olivecrona-077b17b8/"
+          target="_blank"
+          className="bg-white text-gray-700 hover:text-gray-950 p-4 flex items-center gap-2 rounded-full cursor-pointer border border-black/10 focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          href="https://github.com/axxcident"
+          target="_blank"
+          className="bg-white text-gray-700 hover:text-gray-950 text-[1.35rem] p-4 flex items-center gap-2 rounded-full cursor-pointer border border-black/10 focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
