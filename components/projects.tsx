63787052bfe0154c4a7a1e17b3849c85f8723e34
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-10-30 19:40:20 +0100

    finish the header 2:59:41

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Image from "next/image";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
+import Project from "./project";
 
 type ProjectProps = (typeof projectsData)[number];
 
@@ -20,17 +20,36 @@ export default function Projects() {
   );
 }
 
-function Project({ title, description, tags, imageUrl }: ProjectProps) {
-  return (
-    <article className="Bg-gray-100 max-w-[42rem] border-black/5 overflow-hidden sm:pr-8">
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <ul>
-        {tags.map((tag, index) => (
-          <li key={index}>{tag}</li>
-        ))}
-      </ul>
-      <Image src={imageUrl} alt="project I worked on" quality={95} />
-    </article>
-  );
-}
+// function Project({ title, description, tags, imageUrl }: ProjectProps) {
+//   const ref = useRef(null);
+//   useScroll({
+//     target: '',
+//     offset: ["0 1", "1.33 1"]
+//   })
+
+//   return (
+//     <article className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition">
+//       <div className="px-5 pt-3 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[19rem]">
+//         <h3 className="text-2xl font-semibold">{title}</h3>
+//         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+//         <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+//           {tags.map((tag, index) => (
+//             <li
+//               className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+//               key={index}
+//             >
+//               {tag}
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//       <Image
+//         src={imageUrl}
+//         alt="project I worked on"
+//         quality={95}
+//         // Fixa bilderna så att de passar bättre
+//         className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl object-contain group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-105 transition group-even:group-hover:translate-x-3  group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
+//       />
+//     </article>
+//   );
+// }
