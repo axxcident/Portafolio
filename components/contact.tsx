5832a41a51f8f45dad8dd2ea3a5ac3b4947ac206
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2025-07-19 20:13:03 +0200

    cleanup of old email code

--- components/contact.tsx
+++ components/contact.tsx
@@ -5,8 +5,7 @@ import toast from "react-hot-toast";
 import emailjs from '@emailjs/browser';
 import { useSectionInView } from "@/lib/hooks";
 import SectionHeading from "./section-heading";
-import { FaPaperPlane } from "react-icons/fa";
-// import SubmitBtn from "./submit-btn";
+import SubmitBtn from "./submit-btn";
 
 export default function Contact() {
 	const { ref } = useSectionInView("Contact", 0.5);
@@ -78,22 +77,7 @@ export default function Contact() {
 					maxLength={500}
 					disabled={isLoading}
 				/>
-				<button
-					type="submit"
-					className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65"
-					disabled={isLoading}
-				>
-					{isLoading ? (
-						<div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
-					) : (
-					<>
-						Send{" "}
-						<FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
-					</>
-					)}
-
-				</button>
-				{/* <SubmitBtn /> */}
+				<SubmitBtn isLoading={isLoading} />
 			</form>
 		</motion.section>
 	);
